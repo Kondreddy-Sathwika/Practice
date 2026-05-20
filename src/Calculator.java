774c8660ab267082c26f8060b9afb8038bc2e9b3
Kondreddy-Sathwika <Kondreddy.Sathwika@cognizant.com>
2026-05-20 16:56:23 +0530

    Update Calculator.java

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,5 +1,5 @@
 public class Calculator {
     public static void main(String[] args){
-        System.out.println("This is a Calculator class");
+        System.out.println("This is a Calculator");
     }
 }
